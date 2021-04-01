1aeb467374c0f93446a09743e3242230b233fe77
JonOfOz <jon@jonofoz.com>
2021-03-31 19:51:23 -0500

    Add slug field to Post model

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -9,10 +9,11 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=300)
     body = models.TextField()
+    slug = models.SlugField(max_length=200)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
     class Meta:
-        ordering = ['created_at', 'updated_at', 'user', 'title', 'body']
+        ordering = ['created_at', 'updated_at', 'user', 'title', 'body', 'slug']
